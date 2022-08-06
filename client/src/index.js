c9c18428eb218261ce11ba2771ba59b6ab33a867
Jeffrey Liu <yjeffliu@gmail.com>
2022-08-06 03:46:11 -0700

    finished mongodbing

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/Register';
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- server/index.js
+++ server/index.js
@@ -2,15 +2,38 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const User = require('./models/user.model')
 
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:5000');
+mongoose.connect('mongodb://localhost:27017/goodbooks');
 
-app.post('/api/register', (req, res) => {
-    console.log(req.body)
-    res.json({ status: 'ok' })
+app.post('/api/register', async (req, res) => {
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    const user = await User.find({
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    if(user) {
+        return res.json({ status: 'ok', user: true })
+    }
+    else {
+        return res.json({ status: 'error', user: false })
+    }
 })
 
 app.listen(4000, () => {
